119f74f0fab38d911e8f79b745530dd1b09fa844
Paul Mackay <paul.m.mackay85@gmail.com>
2023-02-15 15:37:27 +0000

    Base Types & Primitives

---
+++ basics.ts
@@ -0,0 +1,14 @@
+// Primatives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primatives
+
+let age: number;
+age = 37;
+
+let userName: string;
+userName = "Ash";
+
+let isInstructor: boolean;
+isInstructor = false;
